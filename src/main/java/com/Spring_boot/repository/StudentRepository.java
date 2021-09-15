1f4cbc33ca8f5cd7de37e9f053f5a043c50f014e
TranAnh01236 <trananh12a1vvt@gmail.com>
2021-09-15 07:39:01 +0700

    fix interface repo

--- src/main/java/com/Spring_boot/repository/StudentRepository.java
+++ src/main/java/com/Spring_boot/repository/StudentRepository.java
@@ -1,7 +1,11 @@
 package com.Spring_boot.repository;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.Spring_boot.entity.Student;
 
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer>{
 
 }
