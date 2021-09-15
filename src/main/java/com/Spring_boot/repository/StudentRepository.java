dd0a2eac7329daa502832eb61622a644776eb99e
TranAnh01236 <trananh12a1vvt@gmail.com>
2021-09-15 07:57:29 +0700

    completed jpql-branch

--- src/main/java/com/Spring_boot/repository/StudentRepository.java
+++ src/main/java/com/Spring_boot/repository/StudentRepository.java
@@ -1,11 +1,33 @@
 package com.Spring_boot.repository;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import com.Spring_boot.entity.Student;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer>{
+    @Query(value="select * from student")
+    List<Student> getAllStudents();
 
+
+    @Transactional
+    @Modifying
+    @Query(value="delete student where id = :id")
+    void deleteStudentById(int id);
+
+    @Transactional
+    @Modifying
+    @Query(value="update student set lastname=:lastname, firstname=:firstname, email=:email  where id = :id ")
+    void updateStudentById(int id, String lastname, String firstname, String email);
 }
--- src/test/java/com/Spring_boot/SpringBootDemoApplicationTests.java
+++ src/test/java/com/Spring_boot/SpringBootDemoApplicationTests.java
@@ -1,7 +1,12 @@
 package com.Spring_boot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import com.Spring_boot.repository.StudentRepository;
+import com.Spring_boot.entity.Student;
 
 @SpringBootTest
 class SpringBootDemoApplicationTests {
@@ -9,5 +14,25 @@ class SpringBootDemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	@Autowired
+	StudentRepository studentRepository;
 
+
+	@Test
+	void getAllStudents() {
+		List<Student> students = studentRepository.getAllStudents();
+		System.out.println(students);
+	}
+
+
+	@Test
+	void updateStudentById() {
+		studentRepository.updateStudentById(1, "datma", "lala", "ddddssss@gmail.com");
+	}
+
+
+	@Test
+	void deleteStudentById(int id) {
+		studentRepository.deleteStudentById(1);
+	}
 }
