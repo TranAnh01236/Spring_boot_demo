6de7c546f067cfc8837c10f4f146293baffb02d8
TranAnh01236 <trananh12a1vvt@gmail.com>
2021-09-15 07:45:28 +0700

    completed repo-branch

--- src/test/java/com/Spring_boot/SpringBootDemoApplicationTests.java
+++ src/test/java/com/Spring_boot/SpringBootDemoApplicationTests.java
@@ -1,7 +1,14 @@
 package com.Spring_boot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import com.Spring_boot.entity.Student;
+import com.Spring_boot.repository.StudentRepository;
 
 @SpringBootTest
 class SpringBootDemoApplicationTests {
@@ -9,5 +16,23 @@ class SpringBootDemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	@Autowired
+	private StudentRepository studentRepository;
 
+	@Test
+	void addStudent() {
+		Student student = new Student("thanh", "dat", "assddaaa@gmail.com");
+		studentRepository.save(student);
+	}
+
+	@Test
+	void getStudents() {
+		List<Student> students = studentRepository.findAll();
+		System.out.println(students);
+	}
+
+	@Test
+	void deleteStudentById(int id) {
+		studentRepository.deleteById(1);
+	}
 }
